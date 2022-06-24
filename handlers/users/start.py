ccac5b7f9a5f84812b54ea25552e0183500461b4
ahror0399 <ahrorofficial00@gmail.com>
2022-06-24 14:05:11 +0500

    first

---
+++ handlers/users/start.py
@@ -0,0 +1,16 @@
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import CommandStart
+
+from loader import dp
+
+@dp.message_handler(CommandStart(deep_link='kunuz'))
+async def bot_start(message: types.Message):
+    args = message.get_args()
+    text = f"salom, {message.from_user.full_name} !\n"
+    text += f"Sizni {args} tavsiya qildi"
+    await message.answer(text)
+
+@dp.message_handler(commands='start')
+async def bot_start(message: types.Message):
+    await message.answer(f"Salom, {message.from_user.full_name}!")
+
